fbd49c79521c3643fd78c0d0c403d22e1110e5b3
Bimbo Villaganas <villaganasbimbo143@gmail.com>
2025-06-13 14:28:52 +0800

    config db_connection

--- src/db/db_connection.php
+++ src/db/db_connection.php
@@ -1,14 +1,14 @@
 <?php
-/*$servername = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "cenro_records_db";
 $conn = new mysqli($servername, $username, $password, $dbname);
-*/
-$servername = "	sql107.infinityfree.com";
+
+/*$servername = "sql107.infinityfree.com";
 $username = "if0_39220723";
 $password = "9u8ZFBRRu1O5t4";
-$dbname = "if0_39220723_cenro_records";
+$dbname = "if0_39220723_cenro_records";*/
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
